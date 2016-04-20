ca0919d3fd3f0f71f71ce482f9d3046f091684e5
Thibault Poisson <poissont@laposte.net>
2016-04-20 10:04:39 +0200

    Test

---
+++ class/ign_pay_info.class.php
@@ -0,0 +1,27 @@
+<?php
+class ign_pay_info extends global_class {
+	protected $id;
+	protected $first_name;
+	protected $last_name;
+	protected $email;
+	protected $address;
+	protected $country;
+	protected $state;
+	protected $city;
+	protected $zip;
+	protected $product_id;
+	protected $transaction_id;
+	protected $preapproval_key;
+	protected $product_level;
+	protected $prod_price;
+	protected $status;
+	protected $created_at;
+	
+	
+	public static function identifiant() {
+		return "id";
+	}
+	
+}
+
+
